ce70a67d4bab385700c98b3d664f81b50cdd7a68
gq-su <50959396+gq-su@users.noreply.github.com>
2021-05-15 11:45:22 -0500

    Add files via upload

---
+++ LcrSimulator/MainWindowViewModel.cs
@@ -0,0 +1,131 @@
+﻿using LcrSimulator.Commands;
+using LcrSimulator.Model;
+using Prism.Mvvm;
+using System;
+using System.Linq;
+using System.Windows.Input;
+
+namespace LcrSimulator
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel(LcrGame lcrGame)
+        {
+            LcrGame = lcrGame;
+            PlayGameCommand = new RelayCommand(param => PlayGameCommandHandler(), param => CanExecutePlayGame());
+
+            PropertyChanged += MainWindowViewModel_PropertyChanged;
+        }
+
+        public LcrGame LcrGame { get; set; }
+
+        private int? _playersCount;
+        public int? PlayersCount
+        {
+            get { return _playersCount; }
+            set
+            {
+                if (_playersCount == value)
+                    return;
+
+                _playersCount = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int? _gamesCount;
+        public int? GamesCount
+        {
+            get { return _gamesCount; }
+            set
+            {
+                if (_gamesCount == value)
+                    return;
+
+                _gamesCount = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _minTurns;
+        public int MinTurns
+        {
+            get { return _minTurns; }
+            set
+            {
+                if (_minTurns == value)
+                    return;
+
+                _minTurns = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _maxTurns;
+        public int MaxTurns
+        {
+            get { return _maxTurns; }
+            set
+            {
+                if (_maxTurns == value)
+                    return;
+
+                _maxTurns = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _avgTurns;
+        public int AvgTurns
+        {
+            get { return _avgTurns; }
+            set
+            {
+                if (_avgTurns == value)
+                    return;
+
+                _avgTurns = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void MainWindowViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "PlayersCount" || e.PropertyName == "GamesCount")
+                CanExecutePlayGame();
+        }
+
+
+        #region PlayGameCommand
+        public ICommand PlayGameCommand { get; private set; }
+
+        private bool CanExecutePlayGame()
+        {
+            //Actual project shoulld use message service to validate 
+            //and popup message if failed to meet minmum PlayersCount or GamesCount
+            return PlayersCount > 2 && GamesCount > 2;
+        }
+
+        private void PlayGameCommandHandler()
+        {
+            LcrGame.GamesCount = GamesCount.GetValueOrDefault();
+
+            LcrGame.PlayersCount = PlayersCount.GetValueOrDefault();
+
+            if (LcrGame.Games.Any())
+                LcrGame.Games.Clear();
+
+            for (int i = 0; i < LcrGame.GamesCount; i++)
+            {
+                var game = new Game(LcrGame.PlayersCount);
+                game.PlayGame();
+                LcrGame.Games.Add(game);
+            }
+
+            MinTurns = LcrGame.Games.Min(o => o.TurnsCount);
+            MaxTurns = LcrGame.Games.Max(o => o.TurnsCount);
+            AvgTurns = (int)Math.Round(LcrGame.Games.Average(o => o.TurnsCount), 0);
+        }
+        #endregion
+    }
+}
---
+++ LcrSimulator/Model/Player.cs
@@ -0,0 +1,91 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LcrSimulator.Model
+{
+    public class Player : BindableBase
+    {
+        const int MaxDiceCount = 3;
+
+        public Player()
+        {
+            _chipsCount = MaxDiceCount;
+
+            for(int i = 0; i < MaxDiceCount; i++)
+            {
+                var dice = new Dice { Index = i };
+                Dices.Add(dice);
+            }
+        }
+
+        public int Index { get; set; }
+
+        private int _chipsCount;
+        public int ChipsCount
+        {
+            get { return _chipsCount; }
+            set
+            {
+                if (_chipsCount == value) return;
+
+                var oldCount = _chipsCount;
+                _chipsCount = value < 0 ? 0 : value;
+
+                AdjustDiceList(oldCount, _chipsCount);
+            }
+        }
+
+        public List<Dice> Dices { get; private set; } = new List<Dice>();
+
+        public void Play()
+        {
+            if (Dices.Count < 1)
+                return;
+            
+            Dices.ForEach((dice) => dice.Roll(Index));
+
+            RaisePropertyChanged("Played");
+        }
+
+        private void RemoveDice()
+        {
+            if (Dices.Any())
+                Dices.RemoveAt(Dices.Count - 1);
+        }
+
+        private void AddDice()
+        {
+            if (Dices.Count >= MaxDiceCount)
+                return;
+
+            Dices.Add(new Dice{ Index = Dices.Count });
+        }
+
+        private void AdjustDiceList(int oldChipsCount, int newChipsCount)
+        {
+            bool removeDice = false;
+            int countChange;
+            if (newChipsCount>oldChipsCount)
+               countChange = newChipsCount - oldChipsCount;
+            else
+            {
+                if (oldChipsCount > MaxDiceCount)
+                    countChange = MaxDiceCount - newChipsCount;
+                else
+                    countChange = oldChipsCount - newChipsCount;
+
+                removeDice = true;
+            }
+
+            for (int i = 0; i < Math.Abs(countChange); i++)
+            {
+                if (removeDice)
+                    RemoveDice();
+                else
+                    AddDice(); 
+            }
+        }
+    }
+}
