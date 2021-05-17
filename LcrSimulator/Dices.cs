a96defbce25641b4bd382fb870c276bf546f0346
gq-su <50959396+gq-su@users.noreply.github.com>
2021-05-17 10:42:39 -0500

    Add files via upload

---
+++ LcrSimulator/Dices.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LcrSimulator.Model
+{
+    public class Dices : List<Dice>
+    {
+        public const int MaxDiceCount = 3;
+
+        public Dices()
+        {
+            for (int i = 0; i < MaxDiceCount; i++)
+            {
+                Add(new Dice { Index = i });
+            }
+        }
+    }
+}
