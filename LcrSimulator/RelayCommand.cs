1bc59fd24dafd344b35bed1c2281d8aa50b2c6f7
gq-su <50959396+gq-su@users.noreply.github.com>
2021-05-17 10:48:06 -0500

    Add files via upload

---
+++ LcrSimulator/RelayCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace LcrSimulator.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        #endregion // Fields
+
+        #region Constructors
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute, bool autoCanExecuteUpdating)
+            : this(execute, canExecute)
+        {
+        }
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        /// <summary>
+        /// The following method allows the CommandManager to force the command to raise CanExecuteChanged event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+        #endregion // ICommand Members
+    }
+}
